7b83a2de0f2902ca0f861fd66887fd38cd8eb553
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-02-03 21:18:02 +0330

    using scrapy

--- crawler.py
+++ crawler.py
@@ -1,34 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-import os
+import scrapy
 
-# Create a folder to store the downloaded images
-if not os.path.exists('images'):
-    os.makedirs('images')
+class PexelsSpider(scrapy.Spider):
+    name = "pexels"
+    start_urls = [
+        'https://unsplash.com/s/photos/knife'
+    ]
 
-# Send a GET request to the URL
-response = requests.get('https://www.freepik.com/photos/knife-axe')
-
-# Parse the HTML content
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# Find the image elements using the full XPath
-for idx in range(1, 11):  # Download the first 10 images
-    image_xpath = f"/html/body/main/div[3]/div/div[2]/section/figure[{idx}]/div/a/img"
-    image = soup.find('img', xpath=image_xpath)
-
-    if image:
-        image_url = image['src']
-        try:
-            # Send a GET request to download the image
-            image_response = requests.get(image_url)
-            # Save the image to the 'images' folder
-            with open(f'images/image_{idx}.jpg', 'wb') as f:
-                f.write(image_response.content)
-            print(f'Downloaded image {idx}')
-        except Exception as e:
-            print(f'Error downloading image {idx}: {e}')
-    else:
-        print(f'Image {idx} not found')
-
-print('Download complete!')
+    def parse(self, response):
+        for index in range(1, 11):
+            xpath = f'/html/body/div/div/div[1]/div/div[2]/div[4]/div/div[1]/div[2]/div/div/div[1]/div/div/div[{index}]/figure[2]/div/div[1]/div/div/a/div/div[2]/img'
+            image_url = response.xpath(xpath + '/@src').get()
+            yield {
+                'image_urls': [image_url]
+            }
